a7f169aca6fe596b372f8d4c756eb1f0f335a19f
Phu <33654804+lequangphu@users.noreply.github.com>
2024-04-05 15:47:47 +0700

    Add button to create a new grid, change grid color whenever hovering the mouse over it.

--- script.js
+++ script.js
@@ -4,33 +4,51 @@
         // add 16 rows to the container,
         // add 16 columns to each row.
 // Pseudocode
-    // Create a div element by hand in the index.html file, i.e. the grid.
-    // Create an array of 16 rows.
-    // Create an array of 16 columns.
-    // Add 16 columns to each row.
-    // Add 16 rows to the grid.
+    // Select the grid, a div element created by hand in index.html.
+    // Create a row, which is a div element.
+    // Create a column, which is a div element.
+    // Loop to repeatedly add the column 16 times to the row.
+    // Loop to repeatedly add the row 16 times to the grid.
+    // Change the squares background color when passing the mouse over them.
+    // Select the button, a button element created by hand in index.html.
+    // Whenever the button is clicked:
+        // Promt to ask user inputting the size of the grid, i.e. number of squares.
+        // Clear the existing grid.
+        // Create the new grid.
+        // Limit the grid size to 100 max.
 
-// Function to create the Grid
-
-
-// Function to create a div element
 function createDiv(elementClass) {
     const element = document.createElement('div');
     element.className = elementClass;
     return element;
 }
 
-// Function to add a child node 16 times to a parent node
-function addChildNode(parentNode, childNode) {
-    for (let i = 0; i < 16; i++) {
+function addChildNode(parentNode, childNode, gridSize) {
+    for (let i = 0; i < gridSize; i++) {
         parentNode.appendChild(childNode.cloneNode(true));
     }
-    return parentNode
+    return parentNode;
 }
 
-let grid = document.querySelector('#grid');
-let row = createDiv('row container');
-const column = createDiv('column');
+let theGrid = document.querySelector('#grid');
+const theButton = document.querySelector('button');
 
-row = addChildNode(row, column);
-grid = addChildNode(grid, row);
+theButton.addEventListener('click', () => {
+    
+    theGrid.innerHTML = '';
+
+    let gridSize = parseInt(prompt('Squares per side?', '16'));
+    gridSize = gridSize > 100 ? 100 : gridSize;
+
+    let row = createDiv('row container');
+    const column = createDiv('column');
+
+    row = addChildNode(row, column, gridSize);
+    theGrid = addChildNode(theGrid, row, gridSize);
+})
+
+theGrid.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('column')) {
+        event.target.style.backgroundColor = 'black';
+    }
+})
